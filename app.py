34c039237c9199ed09daf8d1253f28a758005c5c
Navyasri Daiga <navyasridaiga@gmail.com>
2026-05-19 15:07:31 +0530

    Fixed Render host and port

--- app.py
+++ app.py
@@ -239,5 +239,5 @@ import os
 if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000))
+        port=int(os.environ.get("PORT", 10000))
     )
